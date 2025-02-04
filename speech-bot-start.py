c5142980cedaa519d555a4a467744fcb0b266f6c
Sammels <sasagete@gmail.com>
2025-02-04 15:02:53 +0300

    fix: add cred to env.

--- speech-bot-start.py
+++ speech-bot-start.py
@@ -27,12 +27,12 @@ def echo(update: Update, context: CallbackContext):
     "Func Echo return  user message"
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+
 def detect_intent_texts(project_id, session_id, texts, language_code):
     """Returns the result of detect intent with texts as inputs.
 
     Using the same `session_id` between requests allows continuation
     of the conversation."""
-    
 
     session_client = dialogflow.SessionsClient()
 
@@ -59,21 +59,29 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
         print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
 
 
-def main(token: str):
+def main(token: str, GOOGLE_PROJECT_ID: str, GOOGLE_LANGUAGE_CODE: str):
     """Main function running code"""
-    updater = Updater(token=token)
-    dispatcher = updater.dispatcher
+    project_id = GOOGLE_PROJECT_ID
+    session_id = "test-sess"
+    texts = ["Привет"]
+    language_code = GOOGLE_LANGUAGE_CODE
+    detect_intent_texts(project_id, session_id, texts, language_code)
 
-    start_handler = CommandHandler("start", start)
-    dispatcher.add_handler(start_handler)
+    # updater = Updater(token=token)
+    # dispatcher = updater.dispatcher
 
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
+    # start_handler = CommandHandler("start", start)
+    # dispatcher.add_handler(start_handler)
 
-    updater.start_polling()
+    # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    # dispatcher.add_handler(echo_handler)
+
+    # updater.start_polling()
 
 
 if __name__ == "__main__":
     env.read_env()
     TELEGRAM_TOKEN = env("TELEGRAM_TOKEN")
-    main(TELEGRAM_TOKEN)
+    GOOGLE_PROJECT_ID = env("PROJECT_ID")
+    GOOGLE_LANGUAGE_CODE = env("LANGUAGE_CODE")
+    main(TELEGRAM_TOKEN, GOOGLE_PROJECT_ID, GOOGLE_LANGUAGE_CODE)
