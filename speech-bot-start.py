7bd3a91a109c1d07b3a6a9966f24027150db3217
Sammels <sasagete@gmail.com>
2025-02-04 14:46:14 +0300

    feature: add google APi

--- speech-bot-start.py
+++ speech-bot-start.py
@@ -9,6 +9,7 @@ from telegram.ext import (
     Filters,
     CallbackContext,
 )
+from google.cloud import dialogflow
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s- %(message)s", level=logging.INFO
@@ -25,6 +26,37 @@ def start(update: Update, context: CallbackContext):
 def echo(update: Update, context: CallbackContext):
     "Func Echo return  user message"
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    for text in texts:
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+        print("=" * 20)
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
 
 
 def main(token: str):
