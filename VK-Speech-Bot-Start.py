95d483a05bc577ebd4cc9b23c98ea4e23078b462
Sammels <sasagete@gmail.com>
2025-02-12 11:08:43 +0300

    feature: add VK bot

---
+++ VK-Speech-Bot-Start.py
@@ -0,0 +1,21 @@
+from environs import env
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+env.read_env()
+VK_GROUP_TOKEN = env("VK_GROUP_TOKEN")
+print(VK_GROUP_TOKEN)
+
+vk_session = vk_api.VkApi(token=VK_GROUP_TOKEN)
+
+longpoll = VkLongPoll(vk_session)
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        print('Новое сообщение:')
+        if event.to_me:
+            print('Для меня от: ', event.user_id)
+        else:
+            print('От меня для: ', event.user_id)
+        print('Текст:', event.text)
+
