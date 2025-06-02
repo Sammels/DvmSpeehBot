79e73d1ecb2deb7b88b5e439ca3dbc07da91295d
Sammels <sasagete@gmail.com>
2025-06-02 17:59:24 +0300

    fix: Step 3 of 11. Del cli return

--- learning_script.py
+++ learning_script.py
@@ -28,8 +28,6 @@ def create_intent(project_id, display_name, training_phrases_parts, message_text
         request={"parent": parent, "intent": intent}
     )
 
-    print("Intent created: {}".format(response))
-
 
 def main():
     """
@@ -49,6 +47,8 @@ def main():
             files[intent]["questions"],
             files[intent]["answer"],
         )
+        
+
 
 
 if __name__ == "__main__":
